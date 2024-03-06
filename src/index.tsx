ade159bc062c344542e384f0fc107ef9876d8d85
Olya Shyian <olya.shyian.fed@gmail.com>
2024-03-06 14:52:39 +0200

    add minor fixes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
-
 import './styles/globals.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
